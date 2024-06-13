54b1acd955ffba2aae8ba61ac662486d5464f2bc
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2024-06-13 02:00:56 -0500

    refactor: move controllers to infrastructure layer

--- src/main/java/com/altioracorp/evaluation/infrastructure/controllers/ItemController.java
+++ src/main/java/com/altioracorp/evaluation/infrastructure/controllers/ItemController.java
@@ -1,4 +1,56 @@
 package com.altioracorp.evaluation.infrastructure.controllers;
 
+import com.altioracorp.evaluation.application.services.ItemService;
+import com.altioracorp.evaluation.domain.models.Item;
+import org.apache.coyote.Response;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("api/items")
 public class ItemController {
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
+    @PostMapping()
+    public ResponseEntity<Item> createItem(@RequestBody Item item) {
+        Item createdItem = itemService.createItem(item);
+        return new ResponseEntity<>(createdItem, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Item>> getAllItems() {
+        List<Item> items = itemService.allItems();
+        return new ResponseEntity<>(items, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Item> getItemById(@PathVariable Long id) {
+        Optional<Item> item = itemService.getItem(id);
+        return item.map(item_ -> new ResponseEntity<>(item_, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Item> updateItem(@PathVariable Long id, @RequestBody Item item) {
+        return itemService.update(id, item).map(item_ -> new ResponseEntity<>(item, HttpStatus.OK)).
+                orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteItemById(@PathVariable Long id) {
+        if (itemService.delete(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
--- src/main/java/com/altioracorp/evaluation/infrastructure/controllers/OrderController.java
+++ src/main/java/com/altioracorp/evaluation/infrastructure/controllers/OrderController.java
@@ -1,4 +1,56 @@
 package com.altioracorp.evaluation.infrastructure.controllers;
 
+import com.altioracorp.evaluation.application.services.OrderService;
+import com.altioracorp.evaluation.domain.models.Order;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("api/orders")
 public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Order>> getAllOrders() {
+        List<Order> orders = orderService.allOrders();
+        return new ResponseEntity<>(orders, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Order> getOrderById(@PathVariable Long id) {
+        Optional<Order> order = orderService.getOrder(id);
+        return order.map(order_ -> new ResponseEntity<>(order_, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @PostMapping
+    public ResponseEntity<Order> createOrder(@RequestBody Order order) {
+        Order orderCreated = orderService.save(order);
+        return new ResponseEntity<>(orderCreated, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Optional<Order>> updateOrder(@RequestBody Order order, @PathVariable Long id) {
+        Optional<Order> orderUpdated = orderService.update(id, order);
+        return orderUpdated.map(order_ -> new ResponseEntity<>(orderUpdated, HttpStatus.OK))
+                .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteOrderById(@PathVariable Long id) {
+        if (orderService.delete(id)) {
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
 }
