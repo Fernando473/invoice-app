520e45c8045a5ff14ce168c17e0ebe787314a954
Fernando Sangopanta <jhon.sangopanta@epn.edu.ec>
2025-01-03 09:44:44 -0500

    refactor: properties and cross origins

--- src/main/java/com/altioracorp/evaluation/infrastructure/controllers/ItemController.java
+++ src/main/java/com/altioracorp/evaluation/infrastructure/controllers/ItemController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "http://127.0.0.1:5500")
 @RestController
 @RequestMapping("api/items")
 public class ItemController {
